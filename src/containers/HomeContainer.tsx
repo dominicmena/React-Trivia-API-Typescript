5a9dc3816c8fe421149d1c0a05db663bdb26c2ae
dominicmena <mena.dominic@gmail.com>
2024-06-19 00:16:55 -0500

    updated homecontainer to manage game state and fetch questions on game start

--- src/containers/HomeContainer.tsx
+++ src/containers/HomeContainer.tsx
@@ -2,6 +2,7 @@ import APIClient from "../api/client";
 import Placeholder from "../components/Placeholder";
 import { useQuery } from "react-query";
 import { useState } from "react";
+import { Question } from "../types/index";
 
 type Props = {
   apiClient: APIClient;
@@ -9,18 +10,25 @@ type Props = {
 
 export default function HomeContainer(props: Props) {
   const [gameStarted, setGameStarted] = useState(false)
-  const { data: questions = [], isLoading: isLoadingQuestions } = useQuery({
-    queryKey: ["questions"],
-    queryFn: () => props.apiClient.getQuestions(),
-  });
+  const [questions, setQuestions] = useState<Question[]>([])
+  const { isLoading, isError, data } = useQuery("questions", () => props.apiClient.getQuestions(),
+  );
 
-  if (isLoadingQuestions) {
+  const handleStartGame = () => {
+    setGameStarted(true)
+    setQuestions(data)
+  }
+
+  if (isLoading) {
     return "Loading...";
   }
 
-  function handleStartGame() {
-    console.log("Start Game");
-  }
-
-  return <Placeholder questions={questions} onStartGame={handleStartGame} />;
+ if (isError) {
+  return "Error loading questions. Please try again."
+ }
+  return gameStarted ? (
+   <Placeholder questions={questions} onStartGame={handleStartGame} />
+  ) : (
+  <Placeholder questions={[]} onStartGame={handleStartGame}/>
+  )
 }
