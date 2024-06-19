16bb963a3b1c530268ce6a22641c79270504509b
dominicmena <mena.dominic@gmail.com>
2024-06-19 00:08:35 -0500

    added card in placeholder component for instructions and adding useState for homecontainer to start a new game

--- src/components/Placeholder.tsx
+++ src/components/Placeholder.tsx
@@ -29,11 +29,20 @@ export default function Placeholder(props: Props) {
         marginBottom={theme.space_huge}
         width="90%"
       >
-        {PLACEHOLDERS.map((text) => {
-          return <Card key={text}>{text}</Card>;
-        })}
+        <Card marginBottom={theme.space_md}>
+          <h2>Instructions:</h2>
+          <p>
+            Welcome to the Trivia Game! Click the button below to start a new
+            game.
+          </p>
+          <p>Select a category and number of questions before starting the game.</p>
+        </Card>
       </Flex>
       <Button onClick={props.onStartGame}>Example Button</Button>
     </Flex>
   );
 }
+
+// {PLACEHOLDERS.map((text) => {
+//   return <Card key={text}>{text}</Card>;
+// })}
--- src/containers/HomeContainer.tsx
+++ src/containers/HomeContainer.tsx
@@ -1,12 +1,14 @@
 import APIClient from "../api/client";
 import Placeholder from "../components/Placeholder";
 import { useQuery } from "react-query";
+import { useState } from "react";
 
 type Props = {
   apiClient: APIClient;
 };
 
 export default function HomeContainer(props: Props) {
+  const [gameStarted, setGameStarted] = useState(false)
   const { data: questions = [], isLoading: isLoadingQuestions } = useQuery({
     queryKey: ["questions"],
     queryFn: () => props.apiClient.getQuestions(),
